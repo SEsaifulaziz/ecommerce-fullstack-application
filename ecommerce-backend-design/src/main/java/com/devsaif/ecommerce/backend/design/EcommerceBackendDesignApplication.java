7c860e26823838f6f9c4b7f4c45970f2bef24f4e
SaifulAziz <gamestopik712@gmail.com>
2026-05-16 13:27:09 +0500

    Initial commit: Set up frontend and backend workspace structure for integration

---
+++ ecommerce-backend-design/src/main/java/com/devsaif/ecommerce/backend/design/EcommerceBackendDesignApplication.java
@@ -0,0 +1,13 @@
+package com.devsaif.ecommerce.backend.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackendDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackendDesignApplication.class, args);
+	}
+
+}
---
+++ ecommerce-backend-design/src/test/java/com/devsaif/ecommerce/backend/design/EcommerceBackendDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsaif.ecommerce.backend.design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackendDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
