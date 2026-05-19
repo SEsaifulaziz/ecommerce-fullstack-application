cb2ce3bd5ba7634cdf17c1cefa8a30ee78928763
Saiful Aziz <gamestopik712@gmail.com>
2026-05-19 23:37:02 +0500

    Add repository methods for searching products by name, category and combine with pagination

--- e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/repository/ProductRepository.java
+++ e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/repository/ProductRepository.java
@@ -1,8 +1,16 @@
 package com.developerhubcorporation.e_commerce.backend.design.repository;
 
 import com.developerhubcorporation.e_commerce.backend.design.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
+    Page<Product> findByNameContainingIgnoreCaseAndCategoryContainingIgnoreCase(String name, String category,
+                                                                                Pageable pageable);
+
+    Page<Product> findByNameContainingIgnoreCase(String name, Pageable pageable);
+
+    Page<Product> findByCategoryContainingIgnoreCase(String category, Pageable pageable);
 }
