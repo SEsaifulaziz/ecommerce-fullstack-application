2fcf961f143ed5332ffa2e01c68256248a406e2c
Saiful Aziz <gamestopik712@gmail.com>
2026-05-20 21:00:23 +0500

    feat: create Role entity for (RBAC)

---
+++ e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/model/Role.java
@@ -0,0 +1,24 @@
+package com.developerhubcorporation.e_commerce.backend.design.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 20)
+    private String name;
+
+    public Role(String name){
+        this.name = name;
+    }
+}
