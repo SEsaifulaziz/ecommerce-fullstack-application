cee309770b7f5111346515f2c0e4810b686e48a9
Saiful Aziz <gamestopik712@gmail.com>
2026-05-20 13:49:39 +0500

    Add ProductMapper interface for entity-DTO conversion using MapStruct

---
+++ e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/mapper/ProductMapper.java
@@ -0,0 +1,15 @@
+package com.developerhubcorporation.e_commerce.backend.design.mapper;
+
+import com.developerhubcorporation.e_commerce.backend.design.dto.ProductRequestDTO;
+import com.developerhubcorporation.e_commerce.backend.design.model.Product;
+import org.mapstruct.Mapper;
+
+
+// componentModel = "spring" tells MapStruct to generate this as a Spring Bean (@Component)
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    Product toEntity(ProductRequestDTO dto);
+
+    ProductRequestDTO toResponseDTO(Product product);
+}
