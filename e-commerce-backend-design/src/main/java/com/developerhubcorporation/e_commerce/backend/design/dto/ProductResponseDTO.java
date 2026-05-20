a58da1ed5c9039e3a038a1188ec3e82446753517
Saiful Aziz <gamestopik712@gmail.com>
2026-05-20 13:19:17 +0500

    feat: create dto for response

--- e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/dto/ProductResponseDTO.java
+++ e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/dto/ProductResponseDTO.java
@@ -1,4 +1,16 @@
 package com.developerhubcorporation.e_commerce.backend.design.dto;
 
+import com.developerhubcorporation.e_commerce.backend.design.model.Product;
+import lombok.Data;
+
+@Data
 public class ProductResponseDTO {
+
+    private Long id;
+    private String name;
+    private Double price;
+    private String category;
+    private String description;
+    private String image;
+    private Integer stock;
 }
