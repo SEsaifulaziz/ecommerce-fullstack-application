d4a3b303cb796a24156571aea3065a73b67628cd
Saiful Aziz <gamestopik712@gmail.com>
2026-05-20 16:20:23 +0500

    Refactor: optimized ResponseEntity responses

--- e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/controller/ProductController.java
+++ e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/controller/ProductController.java
@@ -29,14 +29,18 @@ public class ProductController {
 
         log.info("REST request to save Product : {}", dto.getName());
 
-        return new ResponseEntity<>(productService.save(dto),HttpStatus.CREATED);
+        ProductResponseDTO savedProduct = productService.save(dto);
+
+        return ResponseEntity.ok().body(savedProduct);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ProductResponseDTO> getProductById(@PathVariable Long id) {
         log.info("REST request to get Product by ID : {}", id);
 
-        return ResponseEntity.ok().body(productService.getById(id));
+        ProductResponseDTO getById = productService.getById(id);
+
+        return ResponseEntity.ok().body(getById);
     }
 
     @GetMapping()
@@ -46,18 +50,18 @@ public class ProductController {
     ) {
         log.info("REST request to get a paginated list of Products");
         Pageable pageable = PageRequest.of(page, size);
-        return ResponseEntity.ok(productService.findAll(pageable));
+        return ResponseEntity.ok().body(productService.findAll(pageable));
     }
 
     @GetMapping("/filter")
-    public ResponseEntity<Page<Product>> getFilteredProducts(
+    public ResponseEntity<Page<ProductResponseDTO>> getFilteredProducts(
             @RequestParam(required = false) String search,
             @RequestParam(required = false) String category,
             @RequestParam(required = false, defaultValue = "0")  int page,
             @RequestParam(required = false, defaultValue = "10") int size
     ) {
         log.info("REST request to filter Products - Search: {}, category: {}", search, category);
-        Page<Product> FilteredProducts = productService.getFilteredProducts(search, category, page, size);
+        Page<ProductResponseDTO> FilteredProducts = productService.getFilteredProducts(search, category, page, size);
         return ResponseEntity.ok().body(FilteredProducts);
     }
 
