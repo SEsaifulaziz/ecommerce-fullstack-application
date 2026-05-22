a369ec65f9c8533d1d0127cc51786830d0d17a84
Saiful Aziz <gamestopik712@gmail.com>
2026-05-23 02:01:16 +0500

    refactor: upgrade ErrorDetails Included status, error definition, and validation fields to the data shape

--- e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/exception/ErrorDetails.java
+++ e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/exception/ErrorDetails.java
@@ -1,20 +1,38 @@
 package com.developerhubcorporation.e_commerce.backend.design.exception;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+
+import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 public class ErrorDetails {
     private LocalDateTime timestamp;
+    private int status;
+    private String error;
     private String message;
     private String details;
+    private Map<String, String> errors; // Key-Value pair for fields (e.g., "email": "Invalid format")
 
+    // Constructor for regular exceptions (like 404 Not Found)
+    public ErrorDetails(LocalDateTime timestamp, int status, String error, String message, String details) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.details = details;
+    }
+
+    // Overloaded constructor for validation exceptions (400 Bad Request)
+    public ErrorDetails(LocalDateTime timestamp, int status, String error, String message, String details, Map<String, String> errors) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.details = details;
+        this.errors = errors;
+    }
 }
