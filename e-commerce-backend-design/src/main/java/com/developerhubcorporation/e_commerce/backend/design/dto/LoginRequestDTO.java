433cbad91f9c702c5ed4a32f865cf238896c0a7d
Saiful Aziz <gamestopik712@gmail.com>
2026-05-22 23:06:37 +0500

    feat: add DTO to handle credentials validations during user sign-in flow

---
+++ e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/dto/LoginRequestDTO.java
@@ -0,0 +1,14 @@
+package com.developerhubcorporation.e_commerce.backend.design.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequestDTO {
+
+    @NotBlank(message = "Username cannot be blank")
+    private String username;
+
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
+}
