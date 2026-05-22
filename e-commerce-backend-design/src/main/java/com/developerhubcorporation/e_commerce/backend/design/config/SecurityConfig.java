8880baebe0120dc2d18c7bafbaa605eb1dbb5cf5
Saiful Aziz <gamestopik712@gmail.com>
2026-05-22 17:14:52 +0500

    feat: add SecurityConfig with BCrypetPasswordEncoder and AuthenticationManager

---
+++ e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.developerhubcorporation.e_commerce.backend.design.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity // Tells Spring to apply our custom security filters globally
+@EnableMethodSecurity // Allows us to use annotations like @PreAuthorize("hasRole('ADMIN')") on controllers later
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+}
