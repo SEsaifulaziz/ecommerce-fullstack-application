c3c0a13084d5c04ece770f998c12a946ce887539
Saiful Aziz <gamestopik712@gmail.com>
2026-05-23 01:21:51 +0500

    feat: Added @PreAuthorize annotation to require ROLE_ADMIN for product insertion

--- e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/controller/ProductController.java
+++ e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/controller/ProductController.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -24,6 +25,7 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping()
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ProductResponseDTO> createProduct(
             @Valid @RequestBody ProductRequestDTO dto) {
 
