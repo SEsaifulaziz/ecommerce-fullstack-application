f277be0744c6ebb076d4c4a3ca745d3d143bce0a
Saiful Aziz <gamestopik712@gmail.com>
2026-05-22 23:01:55 +0500

    feat: add SignupRequestDTO to handle signup field validations

---
+++ e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/dto/SignupRequestsDTO.java
@@ -0,0 +1,28 @@
+package com.developerhubcorporation.e_commerce.backend.design.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class SignupRequestsDTO {
+
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    private String username;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Size(max = 50)
+    @Email(message = "Please provide a valid email address")
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, max = 20, message = "Password must be between 6 and 40 characters")
+    private String password;
+
+    // the frontend will send an array of strings like ["user"] or ["admin"]
+    private Set<String> role;
+}
