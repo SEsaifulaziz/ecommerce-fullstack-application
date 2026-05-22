4aeff5263392020c69c9387749198b52b3482f2c
Saiful Aziz <gamestopik712@gmail.com>
2026-05-22 16:19:23 +0500

    Add user details

--- e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/model/User.java
+++ e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/model/User.java
@@ -1,4 +1,38 @@
 package com.developerhubcorporation.e_commerce.backend.design.model;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String username;
+
+    @Column(unique = true, nullable = false, length = 50)
+    @Email
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new  HashSet<>();
+
 }
