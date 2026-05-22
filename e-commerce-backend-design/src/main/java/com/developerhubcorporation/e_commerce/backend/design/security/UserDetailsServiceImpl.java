df7fd1f8b70fb991aca7a9f344d1d5c03e7d9414
Saiful Aziz <gamestopik712@gmail.com>
2026-05-22 17:07:43 +0500

    implemented loadByUsername

---
+++ e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.developerhubcorporation.e_commerce.backend.design.security;
+
+import com.developerhubcorporation.e_commerce.backend.design.model.User;
+import com.developerhubcorporation.e_commerce.backend.design.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepo;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user =  userRepo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
