1f78c3fe41c78a6820d03496c5fe6007002d91f7
Saiful Aziz <gamestopik712@gmail.com>
2026-05-31 12:23:58 +0500

    feat: introduce AuthService for decoupled auth bussiness logic

--- e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/service/AuthService.java
+++ e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/service/AuthService.java
@@ -1,4 +1,11 @@
 package com.developerhubcorporation.e_commerce.backend.design.service;
 
+import com.developerhubcorporation.e_commerce.backend.design.dto.JwtResponseDTO;
+import com.developerhubcorporation.e_commerce.backend.design.dto.LoginRequestDTO;
+import com.developerhubcorporation.e_commerce.backend.design.dto.SignupRequestsDTO;
+
 public interface AuthService {
+    JwtResponseDTO  authenticateUser(LoginRequestDTO dto);
+    JwtResponseDTO registerUser(SignupRequestsDTO dto);
+    void logoutUser();
 }
