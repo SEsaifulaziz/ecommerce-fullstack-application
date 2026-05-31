e37fe5490e899ff7e002c4bf333de752f2e82275
Saiful Aziz <gamestopik712@gmail.com>
2026-05-31 13:47:35 +0500

    feat: add Duplicate registration exception

---
+++ e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/exception/DuplicateResourceException.java
@@ -0,0 +1,7 @@
+package com.developerhubcorporation.e_commerce.backend.design.exception;
+
+public class DuplicateResourceException extends RuntimeException {
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+}
