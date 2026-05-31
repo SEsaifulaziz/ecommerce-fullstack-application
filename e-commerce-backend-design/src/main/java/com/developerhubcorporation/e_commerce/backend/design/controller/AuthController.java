77120efad84afd4b962d7e36c69ca44474712a31
Saiful Aziz <gamestopik712@gmail.com>
2026-06-01 00:34:29 +0500

    Refactor: remove unused imports

--- e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/controller/AuthController.java
+++ e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/controller/AuthController.java
@@ -3,31 +3,14 @@ package com.developerhubcorporation.e_commerce.backend.design.controller;
 import com.developerhubcorporation.e_commerce.backend.design.dto.JwtResponseDTO;
 import com.developerhubcorporation.e_commerce.backend.design.dto.LoginRequestDTO;
 import com.developerhubcorporation.e_commerce.backend.design.dto.SignupRequestsDTO;
-import com.developerhubcorporation.e_commerce.backend.design.model.Role;
-import com.developerhubcorporation.e_commerce.backend.design.model.User;
-import com.developerhubcorporation.e_commerce.backend.design.repository.RoleRepository;
-import com.developerhubcorporation.e_commerce.backend.design.repository.UserRepository;
-import com.developerhubcorporation.e_commerce.backend.design.security.UserDetailsImpl;
-import com.developerhubcorporation.e_commerce.backend.design.security.jwt.JwtUtils;
 import com.developerhubcorporation.e_commerce.backend.design.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 
 @RestController
 @RequestMapping("/api/v1/auth")
