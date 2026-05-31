9d21619ce2e8690118d4bdf91ff99fda9eac4b16
Saiful Aziz <gamestopik712@gmail.com>
2026-05-31 13:12:33 +0500

    refactor: add required constructors to LoginRequestDTO for programmatic authentication instantiation

--- e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/dto/LoginRequestDTO.java
+++ e-commerce-backend-design/src/main/java/com/developerhubcorporation/e_commerce/backend/design/dto/LoginRequestDTO.java
@@ -1,14 +1,21 @@
 package com.developerhubcorporation.e_commerce.backend.design.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequestDTO {
 
     @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     private String username;
 
     @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, max = 20, message = "Password must be between 6 and 40 characters")
     private String password;
 }
